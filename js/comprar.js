7bc3a91b306d82f270d2ed3b13b24762cd08cf13
diegoccora <diego.ccora98@gmail.comgit config user.name diegoccoraGit add .git config user.email diego.ccora98@gmail.com>
2024-07-03 01:56:11 -0500

    Versión final TF

--- js/comprar.js
+++
@@ -1,4 +0,0 @@
-let botonComprar = document.getElementById("comprarahora");
-botonComprar.addEventListener("click",function(){
-    window.location.href = "selePeru.html"
-})
--- js/unete.js
+++
@@ -1,4 +0,0 @@
-let botonUnete = document.getElementById("unete");
-botonUnete.addEventListener("click", function(){
-    window.location.href = "unete.html"
-})
